41daf54719944d83e04813ce088fb3c0337822ea
b.chirani <chirani124@gmail.com>
2026-04-05 02:40:34 +0100

    Added Quick shopping button

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import { Link } from '@tanstack/react-router';
+import { ShoppingBag } from 'lucide-react';
 
 interface ProductCardProps {
   id: string;
@@ -16,12 +17,15 @@ const ProductCard: React.FC<ProductCardProps> = (props) => {
       params={{ id: props.id }}
       className="p-3 shadow-sm rounded-lg flex flex-col gap-2 bg-white"
     >
-      <figure className="flex items-center justify-center aspect-square w-full overflow-hidden rounded-md">
+      <figure className="group flex items-center justify-center aspect-square w-full overflow-hidden rounded-md relative">
         <img
           src={props.gallery[0]}
           alt={props.name}
           className="w-full hover:scale-80 transition duration-300 object-cover"
         />
+        <div className="invisible group-hover:visible hover:opacity-60 p-3 bg-teal-600 absolute rounded-full top-4 right-4 opacity-80">
+          <ShoppingBag className="text-white" />
+        </div>
       </figure>
 
       <div className="flex flex-col">
