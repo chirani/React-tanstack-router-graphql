6bc36cccef30ab99d926da0b9d2d3e9b14a0e381
b.chirani <chirani124@gmail.com>
2026-04-14 02:05:10 +0100

    Added product attribute vlaue data testid

--- src/components/CartDropdown.tsx
+++ src/components/CartDropdown.tsx
@@ -149,6 +149,7 @@ const CartItem: React.FC<CartItemProps> = (props) => {
                       <button
                         key={item.id}
                         onClick={() => handleSelect(attr.id, item.id)}
+                        data-testid={`prodcut-attribute-${attr.id}-${item.value}`}
                         className={`border-3 p-1 ${
                           selectedAttributes[attr.id] === item.id
                             ? 'border-zinc-900'
@@ -168,6 +169,7 @@ const CartItem: React.FC<CartItemProps> = (props) => {
                       <button
                         key={item.id}
                         onClick={() => handleSelect(attr.id, item.id)}
+                        data-testid={`prodcut-attribute-${attr.id}-${item.value}`}
                         className={`px-3 py-1 border rounded ${
                           selectedAttributes[attr.id] === item.id
                             ? 'bg-black text-white'
